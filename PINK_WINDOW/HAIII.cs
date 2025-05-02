3bb4718b4b137d2a9978f1de23f8c8dde7768b16
- [zenith] - <tokyocplusplus@gmail.com>
2025-05-02 12:42:18 +1000

    Create HAIII.cs
    and change up the naming

---
+++ PINK_WINDOW/HAIII.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace MAIN_FILE {
+  public class Haii {
+    public void haii() {
+      Console.WriteLine("haiii!!");
+    }
+  }
+}
